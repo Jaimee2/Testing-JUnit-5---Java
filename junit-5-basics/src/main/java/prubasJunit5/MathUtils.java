7faec9a3d1992008ad7fefcc144821573e8cec57
Jaime <jaimehiguerasmedina98@gmail.com>
2022-09-09 10:41:36 +0200

    Testing with Junit 5 - Java

---
+++ junit-5-basics/src/main/java/prubasJunit5/MathUtils.java
@@ -0,0 +1,19 @@
+package prubasJunit5;
+
+public class MathUtils {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public double computerCircleArea(double radius){
+        return Math.PI * radius * radius;
+
+    }
+
+    public int divide(int a, int b){
+        return  a/b;
+    }
+
+
+}
