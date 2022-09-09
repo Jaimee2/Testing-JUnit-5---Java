b805054a7d1d36a9fbd635fe4207611006458420
Jaime <jaimehiguerasmedina98@gmail.com>
2022-09-09 11:55:11 +0200

    test de funcion de formato de precios

---
+++ junit-5-basics/src/main/java/MonyUtil.java
@@ -0,0 +1,22 @@
+import java.text.DecimalFormat;
+
+public class MonyUtil {
+    public static String format(double money) {
+
+        String simbolo = "$";
+        if (money < 0) {
+            simbolo = "-$";
+            money = money * -1;
+        }
+
+        return simbolo + String.format("%.2f", money);
+    }
+
+    public static String format(double money, String simbolo) {
+        if (money < 0) {
+            simbolo = "-" + simbolo;
+            money = money * -1;
+        }
+        return simbolo + String.format("%.2f", money);
+    }
+}
---
+++ junit-5-basics/src/test/java/MonyUtilTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonyUtilTest {
+
+    @Test
+    public void moneyTest(){
+        String money = MonyUtil.format(1000);
+        assertEquals("$1000,00" , money);
+    }
+
+    @Test
+    public void moneyTestNegative(){
+        String money = MonyUtil.format(-1000);
+        assertEquals("-$1000,00" , money);
+    }
+
+    @Test
+    public void euroMoneyTest(){
+        String money = MonyUtil.format(1000, "€");
+        assertEquals("€1000,00" , money);
+    }
+
+    @Test
+    public void euroMoneyTestNegative(){
+        String money = MonyUtil.format(-1000, "€");
+        assertEquals("-€1000,00" , money);
+    }
+}
